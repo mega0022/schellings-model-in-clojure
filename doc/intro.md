d822320cfba301cd8211aed485e4d8edde46965a
Nic McPhee <mcphee@morris.umn.edu>
2015-10-04 20:15:30 -0500

    Created project with `lein new`

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to schellings-model-in-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/schellings_model_in_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns schellings-model-in-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [schellings-model-in-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
