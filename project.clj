d15d3aa104dbfb786957a80faff101f1b079581d
Nic McPhee <mcphee@morris.umn.edu>
2015-10-04 21:06:03 -0500

    Added the Seesaw dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [seesaw "1.4.2" :exclusions [org.clojure/clojure]]]
   :main ^:skip-aot schellings-model-in-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
