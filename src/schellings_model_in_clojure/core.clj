675a50b9772d7d898353604e32b3f9eb557d5c4e
Nic McPhee <mcphee@morris.umn.edu>
2015-10-06 23:32:30 -0500

    Got all the watchers and binding wired up successfully
    Took lots of fiddling, and the debugging was a bit of a nightmare, but I think I eventually got everything hooked up so we can start the project proper.

--- src/schellings_model_in_clojure/core.clj
+++ src/schellings_model_in_clojure/core.clj
@@ -1,6 +1,7 @@
 (ns schellings-model-in-clojure.core
   (:require
    [seesaw.core :as sc]
+   [schellings-model-in-clojure.model :as model]
    [schellings-model-in-clojure.view :as view]
    [schellings-model-in-clojure.controller :as controller])
   (:gen-class))
@@ -8,12 +9,22 @@
 (defn show-gui []
   (sc/native!)
   (sc/show! (sc/pack! view/main-window))
-  (view/init-view)
-  (view/repaint!)
   (controller/setup-listeners)
   )
 
 (defn -main
   "Start up the simulation of Schelling's model"
   [& args]
-  (show-gui))
+  (show-gui)
+  ;; You should remove everything from here down. I only
+  ;; included this to "force" some sort of change in the
+  ;; system so I could confirm that all my watching and
+  ;; binding was wired up correctly.
+  (let [f (first (deref model/positions))
+        fi @f
+        s (second (deref model/positions))
+        si @s]
+    (Thread/sleep 1000)
+    (swap! f (fn [_] si))
+    (swap! s (fn [_] fi)))
+  )
--- src/schellings_model_in_clojure/model.clj
+++ src/schellings_model_in_clojure/model.clj
@@ -1,11 +1,18 @@
 (ns schellings-model-in-clojure.model)
 
+(def default-similarity 0.3)
+(def default-balance 0.5)
+(def default-empty 0.1)
+
 ; Atoms that reflect the state of the GUI sliders
 ; These aren't good names (they probably shouldn't say
 ; "atom", for example) so feel free to make them better.
-(def similarity-atom (atom 0))
-(def balance-atom (atom 0))
-(def empty-atom (atom 0))
+(def similarity-atom (atom default-similarity))
+(def balance-atom (atom default-balance))
+(def empty-atom (atom default-empty))
+
+(def population (atom []))
+(def positions (atom []))
 
 (defn handle-neighbor-change
   "Called when the state of a neighboring position changes.
@@ -22,14 +29,38 @@
   ; probably where you want to call send to handle whatever
   ; needs to be done. Otherwise everything will end up happening in
   ; the main thread.
-  (println (str "My neighbor changed from " old-state " to " new-state)))
+  (println (str "I am " me " and my neighbor " neighbor " (key " key ") changed from " old-state " to " new-state)))
 
+;; You may be able to leave this alone, but feel free to change it
+;; if you decide to structure your solution differently.
 (defn make-position
   "Create a position atom that contains an individual agent, or nil
   if there's no individual there."
+  []
   (if (< (rand) @empty-atom)
     (atom nil)
     (let [color (if (< (rand) @balance-atom) :red :blue)
-          individual (agent color)]
-      (atom individual))))
+          individual (agent color)
+          position (atom individual)]
+      ; I need to have all the individuals together in
+      ; a collection so I can `send` them all a "message"
+      ; when, e.g., we hit the "Start" button.
+      (swap! population conj individual)
+      ; I'm not sure if I need all the positions, actually,
+      ; but I found that useful for debugging.
+      (swap! positions conj position)
+      position)))
 
+(defn extract-color
+  "Takes an individual agent and returns the color of the individual
+   at that position. You'll need to implement this so that it returns
+   desired color, or 'white' if there's no individual there."
+  [position]
+  ; This returns a totally random color so it should be quite
+  ; obvious if you haven't dealt with this. You can specify colors
+  ; with things like strings ("blue") or keywords (:red).
+  (seesaw.color/color (rand-int 256)
+                      (rand-int 256)
+                      (rand-int 256)
+                      (rand-int 256))
+  )
